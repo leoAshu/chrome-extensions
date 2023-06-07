579822cc5541958a506b30357e7ad7f8712861a4
leoAshu <ashutosh.ojha2009@gmail.com>
2023-06-07 09:54:18 -0700

    tv-show-search: implements google search of selected text via context menus

--- tv-show-search/background.js
+++ tv-show-search/background.js
@@ -7,20 +7,10 @@ chrome.runtime.onInstalled.addListener((details) => {
 
   chrome.contextMenus.onClicked.addListener((event) => {
     console.log(event);
-  });
-
-  chrome.contextMenus.create({
-    title: "Test Context Menu 1",
-    id: "contextMenu2",
-    parentId: "contextMenu1",
-    contexts: ["page", "selection"],
-  });
-
-  chrome.contextMenus.create({
-    title: "Test Context Menu 2",
-    id: "contextMenu3",
-    parentId: "contextMenu1",
-    contexts: ["page", "selection"],
+    chrome.search.query({
+      disposition: "NEW_TAB",
+      text: event.selectionText ?? "TV Shows",
+    });
   });
 });
 
