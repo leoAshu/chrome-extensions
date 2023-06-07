30ed588279ad25a787bbe80d6cf6973b53d7d8f8
leoAshu <ashutosh.ojha2009@gmail.com>
2023-06-07 14:16:13 -0700

    react-ext-webpack: implements options page, adds icon and updates manifest & webpack config

--- react-extension/webpack.config.js
+++ react-extension/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const HtmlPlugin = require('html-webpack-plugin')
+const { Chunk } = require('webpack')
 
 module.exports = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
   entry: {
     popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
   },
   module: {
     rules: [
@@ -21,16 +23,13 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve('src/manifest.json'),
+          from: path.resolve('src/static'),
+
           to: path.resolve('dist'),
         },
       ],
     }),
-    new HtmlPlugin({
-      title: 'React Extension',
-      filename: 'popup.html',
-      chunks: ['popup'],
-    }),
+    ...getHtmlPlugins(['popup', 'options']),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
@@ -39,4 +38,20 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve('dist'),
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: 'React Extension',
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  )
 }
