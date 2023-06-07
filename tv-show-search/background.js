fcdef56b19ffd993407729e992f0949176239d43
leoAshu <ashutosh.ojha2009@gmail.com>
2023-06-07 10:45:24 -0700

    tv-show-search: implements data fetching from APIs

--- tv-show-search/background.js
+++ tv-show-search/background.js
@@ -1,23 +1,22 @@
 chrome.runtime.onInstalled.addListener((details) => {
+  chrome.storage.local.set({
+    shows: [],
+  });
+
   chrome.contextMenus.create({
-    title: "Test Context Menu",
+    title: "Search TV Show",
     id: "contextMenu1",
     contexts: ["page", "selection"],
   });
 
   chrome.contextMenus.onClicked.addListener((event) => {
-    console.log(event);
-    chrome.search.query({
-      disposition: "NEW_TAB",
-      text: event.selectionText ?? "TV Shows",
-    });
+    fetch(`https://api.tvmaze.com/search/shows?q=${event.selectiontext}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        chrome.storage.local.set({
+          shows: data,
+        });
+      });
   });
-});
-
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  console.log(msg);
-  console.log(sender);
-  console.log(sendResponse);
-  sendResponse("Received message from background");
-  chrome.tabs.sendMessage(sender.tab.id, "Got your message from background!");
 });
